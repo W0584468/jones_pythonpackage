62bebf941f9522ca54b907b251abe4d85f6368ef
DenzelJ <denzel.jones@selu.edu>
2018-11-20 19:29:03 +0000

    premerge

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
-setup(name='Jones Python Package',
+from setuptools import setup
+
+setup(name='DJonesPythonPackage',
       version='0.1',
       description='Read a matrix and run BLAST searches',
       url='TBD',
-      author='April',
+      author='Denzel Jones',
       author_email='denzel.jones@selu.edu',
       license='MIT',
-      packages=['jones_pythonproject'],
+      packages=['djonespythonpackage'],
       install_requires=[
           'dendropy',
           'pandas',
           'biopython'
+          
        
       ],
       long_description=open('README.txt').read(),
